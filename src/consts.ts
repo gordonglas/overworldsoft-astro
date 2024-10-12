0d9d427a47b99a5f8c46e903eea88e01650ef039
gglas <gordonglas@gmail.com>
2024-10-12 18:10:45 -0500

    Finish conversion to astro.
    -Add bootstrap, css, fonts, favicons with manifest.
    -Components: layouts, header, footer, comments, RespImg, RespImgInternal.
    -Html/bootstrap/styles for all pages.
    -More posts link to All posts page.

--- src/consts.ts
+++ src/consts.ts
@@ -3,10 +3,11 @@
 
 export const SITE_TITLE = 'Overworld Software';
 export const SITE_DESCRIPTION = 'An indie game studio working on a game engine.';
-export const MAX_POSTS_INDEX = 8;
+export const MAX_POSTS_NEWS_INDEX = 8;
 export const MAX_POSTS_RSS  = 50;
 
 export const appSettings = {
+  company: "Overworld Software",
   formatTitle: function(pageTitle: string): string {
     return pageTitle + " - " + SITE_TITLE;
   }
